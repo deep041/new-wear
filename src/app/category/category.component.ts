e7b9dcdbe125b4350b565b1dbc7509f2a8658fdf
Deep Patel <patel.deep041@gmail.com>
2024-01-29 21:00:00 +0530

    Category page added

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,15 +1,124 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+    selector: 'app-category',
+    templateUrl: './category.component.html',
+    styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+    products: any;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+        this.getProducts();
+    }
+
+    getProducts() {
+        this.products = [
+            {
+                category: 'Classic',
+                products: [
+                    {
+                        name: 'Classic T-Shirt | Let\'s do it',
+                        price: 500,
+                        img: '../../../assets/product1.png',
+                        color: ['Black', 'Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Om',
+                        price: 500,
+                        img: '../../../assets/product2.png',
+                        color: ['Black', 'White', 'Teal']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Just keep moving forward',
+                        price: 500,
+                        img: '../../../assets/product3.png',
+                        color: ['Black', 'White']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product1.png',
+                        color: ['Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product2.png',
+                        color: ['Black', 'White', 'Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product3.png',
+                        color: ['Black']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Let\'s do it',
+                        price: 500,
+                        img: '../../../assets/product1.png',
+                        color: ['Black', 'Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Om',
+                        price: 500,
+                        img: '../../../assets/product2.png',
+                        color: ['Black', 'White', 'Teal']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Just keep moving forward',
+                        price: 500,
+                        img: '../../../assets/product3.png',
+                        color: ['Black', 'White']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product1.png',
+                        color: ['Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product2.png',
+                        color: ['Black', 'White', 'Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product3.png',
+                        color: ['Black']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Let\'s do it',
+                        price: 500,
+                        img: '../../../assets/product1.png',
+                        color: ['Black', 'Teal', 'Red']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Om',
+                        price: 500,
+                        img: '../../../assets/product2.png',
+                        color: ['Black', 'White', 'Teal']
+                    },
+                    {
+                        name: 'Classic T-Shirt | Just keep moving forward',
+                        price: 500,
+                        img: '../../../assets/product3.png',
+                        color: ['Black', 'White']
+                    },
+                    {
+                        name: 'Classic T-Shirt Black L',
+                        price: 500,
+                        img: '../../../assets/product1.png',
+                        color: ['Teal', 'Red']
+                    }
+                ]
+            }
+        ];
+    }
 
 }
--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ProductComponent } from './product/product.component';
+import { RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 const components = [
     HeaderComponent,
-    ProductComponent
+    ProductComponent,
+    CartComponent        
 ];
 
 @NgModule({
     declarations: [
-        ...components        
+        ...components
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule
     ],
     exports: [
         ...components
